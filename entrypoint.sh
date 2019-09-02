543197c77737db08ba402e60eaed0a42f609ca88
Maxim Yefremov <maxYefr@gmail.com>
2019-09-02 09:33:09 +0300

    added wallet 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ex
-ganache-cli -a 3 --acctKeys="/root/ganache-accounts.json" &
+ganache-cli -a 4 --acctKeys="/root/ganache-accounts.json" &
 sleep 3
 node /root/set-public-private-key-strings-in-config.js
-cat /root/playbook/playbook.yml | grep WALLETS -A 15
+cat /root/playbook/playbook.yml | grep WALLETS -A 20
 ethereum-playbook -f /root/playbook/playbook.yml eth-balances
 ethereum-playbook -f /root/playbook/playbook.yml deploy-lottery
 echo ---------------------------------------------------------------
