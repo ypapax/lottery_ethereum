177705517baa4fd781b4a780d2e8d85113a86db5
Maxim Yefremov <maxYefr@gmail.com>
2019-09-02 09:26:49 +0300

    print balances

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ ganache-cli -a 3 --acctKeys="/root/ganache-accounts.json" &
 sleep 3
 node /root/set-public-private-key-strings-in-config.js
 cat /root/playbook/playbook.yml | grep WALLETS -A 15
+ethereum-playbook -f /root/playbook/playbook.yml eth-balances
 ethereum-playbook -f /root/playbook/playbook.yml deploy-lottery
+echo ---------------------------------------------------------------
+ethereum-playbook -f /root/playbook/playbook.yml eth-balances
+ethereum-playbook -f /root/playbook/playbook.yml make-transfers
+ethereum-playbook -f /root/playbook/playbook.yml eth-balances
 while sleep 3600; do :; done
 
